222706abfd25c5c2f57f901ad4c1e577bd169fc6
milenakhv <milenakhv@mail.ru>
2022-12-31 12:11:40 +1000

    нов

--- n_token.py
+++ n_token.py
@@ -1,3 +1,3 @@
-token_group = "vk1.a.I4DTHqcuKaXxbM6fN0eNUAIdbEpFRqr5rC-z0OLmiz7st0jhiV1M33JPFztGcVJPiOBuWAJHXa01kOQUVNH6TzUK1zIIRiYbnmnAsCzDlTuZ61TKSbQ7Zmd4f4EsZurDU_l7vdjEH-hayNLwV8w69GeuMfxjKMOHMUC618GUqcjBWq_YA4hpEF_-TDeniWEJiEsku0sVte8CoBPiBlZdow "
-token_my = "vk1.a.kJz_91zblZv1suNg2FMqemacDDVk1vA7L1-N-cXX1qJXBBYmXEP7vq9I5Bg3SopsfsZtn9m2Do_gi2HCEkPNebkDnJXPUnax17MAYMxDqYVlEdkCAgoQBOKuEGXhmjrEf2Er1NbPzw-Z6trU5HOIsUE7hXgoVxiiRV_TFCMfz6FTeJvd5lTXWC7Xq08l8R8GbcyJ_5dXVBn_bTGIIGhwSg "
+token_group = " "
+token_my = " "
 
